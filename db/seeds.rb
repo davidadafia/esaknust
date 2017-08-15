5344835ff6f84eeb4dfa5697845859b86261b2a6
David Adafia <davidadafia@gmail.com>
2017-08-15 14:20:56 +0100

    ae

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'selorm@gmail.com', password: 'Dave1234567', password_confirmation: 'Dave1234567') if Rails.env.development?
+User.create!(email: 'selorm@gmail.com', password: 'Dave1234567', password_confirmation: 'Dave1234567') if Rails.env.development?
